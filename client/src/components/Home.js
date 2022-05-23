65bf7ac7c5662f189ff902165861821eb14dc718
Ayo Afolabi <ayo.afolabi.24@gmail.com>
2022-05-23 16:40:35 +0100

    register/login fix in insomnia

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 import axios from 'axios'
 
-import { Container } from 'react-bootstrap'
+//import { Container } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -14,35 +14,36 @@ User = get_user_model()
 # Create your views here.
 class RegisterView(APIView):
 
-  def post(self, request):
-    user_to_create = UserSerializer(data=request.data)
-    try:
-      user_to_create.is_valid()
-      user_to_create.save()
-      return Response(user_to_create.data, status=status.HTTP_201_CREATED)
-    except:
-      # print(user_to_create.errors)
-      return Response('Failed to create user', status=status.HTTP_422_UNPROCESSABLE_ENTITY)  
+    def post(self, request):
+        user_to_create = UserSerializer(data=request.data)
+        try:
+            user_to_create.is_valid()
+            user_to_create.save()
+            return Response(user_to_create.data, status=status.HTTP_201_CREATED)
+        except:
+            # print(user_to_create.errors)
+            return Response('Failed to create user', status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 
 class LoginView(APIView):
 
-  def post(self, request):
-      print(request.data)
-      try:
-          user_to_login = User.objects.get(email=request.data.get('email'))
-      except User.DoesNotExist:
-          return PermissionDenied(details='Unauthorised')    
-      if not user_to_login.check_password(request.data.get('password')):
-          return PermissionDenied(details='Unauthorised')
-      dt = datetime.now() + timedelta(days=14)  
-      print('DT --->', int(dt.strftime('%s')))
-      token = jwt.encode({
-        'sub': user_to_login.id,
-        'exp': int(dt.strftime('%s'))
-      }, settings.SECRET_KEY, 'HS256')
-      print('TOKEN -->', token)
-      # print(user_to_login, id)    
-      return Response({
-          'token': token,
-          'message': f'Welcome back {user_to_login.first_name}'
-      }, status.HTTP_202_ACCEPTED)
+    def post(self, request):
+        print(request.data)
+        try:
+            user_to_login = User.objects.get(email=request.data.get('email'))
+        except User.DoesNotExist:
+            return PermissionDenied(details='Unauthorised')
+        if not user_to_login.check_password(request.data.get('password')):
+            return PermissionDenied(details='Unauthorised')
+        dt = datetime.now() + timedelta(days=14)
+        print('DT --->', int(dt.strftime('%s')))
+        token = jwt.encode({
+            'sub': user_to_login.id,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, 'HS256')
+        print('TOKEN ----->', token)
+
+        return Response({
+            'token': token,
+            'message': f'Welcome back {user_to_login.username}'
+        }, status.HTTP_202_ACCEPTED)
