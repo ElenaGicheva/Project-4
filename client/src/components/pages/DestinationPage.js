19aded9be14b742658b5cecfeae2b0dc26652472
Ayo Afolabi <ayo.afolabi.24@gmail.com>
2022-05-27 22:20:55 +0100

    Fixed routes issue

--- client/src/components/pages/DestinationPage.js
+++ client/src/components/pages/DestinationPage.js
@@ -7,26 +7,28 @@ const DestinationPage = () => {
   const [destination, setDestination] = useState([])
   const [hasError, setHasError] = useState({ error: false, message: '' })
 
+  const { continentId } = useParams()
   const { destinationId } = useParams()
   console.log(destinationId)
 
   useEffect(() => {
     const getDestinations = async () => {
       try {
-        const { data } = await axios.get(`/destinations/${destinationId}/`)
+        const { data } = await axios.get(`/destinations/${continentId}/destinations/${destinationId}/`)
         setDestination(data)
       } catch (err) {
         setHasError({ error: true, message: err.message })
       }
     }
     getDestinations()
-  }, [destinationId])
+  }, [continentId, destinationId])
 
   useEffect(() => {
     console.log(destination)
   })
   return (
-    <h2>{destination.name}</h2>
+    <><h2>{destinationId.name}</h2>
+      <h1>Working Now</h1></>
   )
 }
 export default DestinationPage
